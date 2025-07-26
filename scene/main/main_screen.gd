730a549b5e2422825734f316806ea3e84997fac9
Raine Adams <rainedoescode@gmail.com>
2025-07-26 08:27:08 -0500

    Add sprite for player character

---
+++ scene/main/main_screen.gd
@@ -0,0 +1,10 @@
+extends Node
+
+func _ready() -> void:
+	_create_pc()
+
+
+func _create_pc() -> void:
+	var pc: Sprite2D = preload("res://sprite/pc.tscn").instantiate()
+	pc.position = Vector2i(50, 50)
+	add_child(pc)
